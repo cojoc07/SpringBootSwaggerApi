65cd9d509d02195a64f3031fbd1025414d62f55f
Stefan-George Cojocaru <cojoc07@gmail.com>
2019-10-12 16:31:54 +0300

    updating for m2 test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,10 +4,10 @@ INSERT INTO users VALUES (3, 'johnny@gmail.com', 'Johnny Test', '12345678','john
 INSERT INTO postari VALUES (1, 'Exemplu de postare pe forum', NOW());
 INSERT INTO postari VALUES (2, 'Alta postare pe forum', NOW());
 INSERT INTO postari VALUES (3, 'Inca o postare pe forum', NOW());
-INSERT INTO comenzi VALUES (1, "Subscriptie 2 ani", 380, 1);
-INSERT INTO comenzi VALUES (2, "Subscriptie 1 an", 250, 2);
-INSERT INTO comenzi VALUES (3, "Subscriptie 1 luna", 99, 1);
+INSERT INTO comenzi VALUES (1, 'Subscriptie 2 ani', 380, 1);
+INSERT INTO comenzi VALUES (2, 'Subscriptie 1 an', 250, 2);
+INSERT INTO comenzi VALUES (3, 'Subscriptie 1 luna', 99, 1);
 INSERT INTO categorii VALUES (1, 'Financiar');
 INSERT INTO categorii VALUES (2, 'Auto');
 INSERT INTO categorii VALUES (3, 'Tehnologie');
-UPDATE hibernate_sequence SET next_val = 4;
+--mysql only UPDATE hibernate_sequence SET next_val = 4;
