278c048a89b39b9ddf50f539b1bdf7ad4150ef37
Stefan-George Cojocaru <cojoc07@gmail.com>
2019-10-12 17:11:49 +0300

    fixed sql query for h2 db

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -10,4 +10,4 @@ INSERT INTO comenzi VALUES (3, 'Subscriptie 1 luna', 99, 1);
 INSERT INTO categorii VALUES (1, 'Financiar');
 INSERT INTO categorii VALUES (2, 'Auto');
 INSERT INTO categorii VALUES (3, 'Tehnologie');
---UPDATE hibernate_sequence SET next_val = 4;
+--mysql only UPDATE hibernate_sequence SET next_val = 4;
